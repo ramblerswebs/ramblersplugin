025e206d4f1a25a34394ac560c17c12ea72ab278
Ramblers Webs <webmaster@ramblers-webs.org.uk>
2016-11-30 14:45:55 +0000

    Update ramblers.php

--- ramblers.php
+++ ramblers.php
@@ -1,28 +1,29 @@
-<?php
+<<?php
+
 /**
  * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later.
  */
- 
 defined('_JEXEC') or die;
- 
+
 /**
  * Mylib plugin class.
  *
  * @package     Joomla.plugin
  * @subpackage  System.mylib
  */
-class plgSystemRamblers extends JPlugin
-{
+class plgSystemRamblers extends JPlugin {
+
     /**
      * Method to register custom library.
      *
      * return  void
      */
-    public function onAfterInitialise()
-    {
-        if (file_exists(JPATH_BASE . '/ramblers')) {
-              JLoader::registerPrefix('R', JPATH_BASE . '/ramblers');
+    public function onAfterInitialise() {
+
+        if (file_exists(JPATH_SITE . '/ramblers')) {
+            JLoader::registerPrefix('R', JPATH_SITE . '/ramblers');
         }
     }
+
 }
