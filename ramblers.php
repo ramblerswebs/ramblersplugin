33afc498df6223fd3dddb0352b5c104ace9b0629
Ramblers Webs <webmaster@ramblers-webs.org.uk>
2020-05-19 17:09:42 +0100

    Rename old version

--- ramblers.php
+++ ramblers.php
@@ -4,7 +4,7 @@
  * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later.
  */
-defined('_JEXEC') or die; 
+defined('_JEXEC') or die;
 
 /**
  * Mylib plugin class.
@@ -20,6 +20,9 @@ class plgSystemRamblers extends JPlugin {
      * return  void
      */
     public function onAfterInitialise() {
+        if (file_exists(JPATH_SITE . '/ramblers')) {
+            rename( JPATH_SITE . '/ramblersOLD', JPATH_SITE . '/ramblers') ;
+        }
         if (file_exists(JPATH_LIBRARIES . '/ramblers')) {
             JLoader::registerPrefix('R', JPATH_LIBRARIES . '/ramblers');
         }
